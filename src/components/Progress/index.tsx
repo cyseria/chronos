3d4db8afb8c6009a6786894a2fadbc6725a2a06a
cyseria <xcyseria@gmail.com>
2020-03-25 13:24:05 +0800

    wip: 修改配色 & 新增部分 svg icon

---
+++ src/components/Progress/index.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import './index.scss';
+import { Icon } from '@befe/brick-hi';
+import { SvgWaixingren } from '../../images/icon';
+// import './a'
+interface ProgressProps {
+    /**
+     * 标题
+     */
+    title: string;
+    /**
+     * 起点文字
+     */
+    beginText: string;
+    /**
+     * 终点文字
+     */
+    endText: string;
+    /**
+     * 提示文字
+     */
+    notes: string;
+    /**
+     * 当前进度
+     */
+    progress: number;
+}
+
+export const Progress: React.FC<ProgressProps> = props => {
+    return (
+        <div className="progress">
+            <h3 className="progress-title">
+                <span>{props.title}</span>
+                <span>{props.progress}%</span>
+            </h3>
+            <div className="progress-content">
+                <span className="progress-begin-text">{props.beginText}</span>
+                <div className="progress-line">
+                    <div className="progress-current">
+                        <div className="tool-tips">{props.notes}</div>
+                        <Icon svg={SvgWaixingren} className="person-icon"/>
+                    </div>
+                    <div className="progress-line-range" />
+                </div>
+                <span className="progress-end-text">{props.endText}</span>
+            </div>
+        </div>
+    );
+};
